4832fc7a504afb2550946c5a6169a9f949036d15
Eltink <glaucoeltink.97@gmail.com>
2023-10-28 15:37:29 +0200

    Updating to status v7_2

--- main.py
+++ main.py
@@ -1,112 +1,193 @@
 import tkinter as tk
-from PIL import ImageTk, Image
+from tkinter import filedialog
+from PIL import Image, ImageTk
+from PIL.ExifTags import TAGS
 import os
 from copiador_de_arquivo import copia_arquivo
 
-class ImageViewer:
-    def __init__(self, root):
-        self.root = root
-        self.image_label = tk.Label(root)
-        self.file_info_text = tk.StringVar()
-        self.loaded_files = {}
-        self.index_atual = 0
-        self.source_dir = 'C:\\Users\\glauc\\Desktop\\sel'
-        self.destination_dir = 'C:\\Users\\glauc\\Desktop\\sel'
-        self.valid_extensions = [".jpg", ".jpeg", ".png", ".gif", ".JPG"]
-        self.zoom_factor = 1.0
+# Initialize the GUI
+root = tk.Tk()
+imagem_mostrada = tk.Label(root)
+imagem_mostrada.pack()
+index_atual = 0
 
-    def setup_gui(self):
-        self.root.title("Eu amo o Bernado")
-        self.root.state('zoomed')
+# Set up the window
+root.title("Eu amo o Bernado")
+root.state('zoomed')
 
-        file_info_label = tk.Label(self.root, textvariable=self.file_info_text, font=('Arial', 12))
+# Set up the file information display
+file_info_visible = True
+file_info_text = tk.StringVar()
+file_info_text.set("init\nisso ai")
+file_info_label = tk.Label(root, textvariable=file_info_text, font=('Arial', 12))
+file_info_label.place(anchor=tk.NW)
+
+
+# Toggle file information display
+def toggle_file_info(event):
+    global file_info_visible
+    file_info_visible = not file_info_visible
+    if file_info_visible:
         file_info_label.place(anchor=tk.NW)
+    else:
+        file_info_label.place_forget()
 
-        self.root.bind("6", self.next_image)
-        self.root.bind("4", self.previous_image)
-        self.root.bind("8", self.copy_image)
-        self.root.bind("<MouseWheel>", self.zoom_image)
 
-    def run(self):
-        self.load_images()
-        self.display_image(self.index_atual)
-        self.root.mainloop()
+# Define source and destination directories
+#source_dir = 'C:\\Users\\glauc\\Desktop\\sel'
+source_dir = filedialog.askdirectory()
+destination_dir = 'C:\\Users\\glauc\\Desktop\\sel'
 
-    def load_images(self):
-        files = [file for file in os.listdir(self.source_dir) if any(file.endswith(ext) for ext in self.valid_extensions)]
-        for file in files:
-            try:
-                self.loaded_files[file] = self.load_image(file)
-            except FileNotFoundError:
-                print(f"Error: File {file} not found in {self.source_dir}")
-        if not self.loaded_files:
-            messagebox.showinfo("No Images Found", "No valid image files found in the source directory.")
+# Define valid image file extensions
+valid_extensions = [".jpg", ".jpeg", ".png", ".gif", ".JPG"]
 
-    def load_image(self, filename):
-        img = Image.open(os.path.join(self.source_dir, filename))
-        return ImageTk.PhotoImage(img)
+# Get a list of files with valid extensions in the source directory
+files = [file for file in os.listdir(source_dir) if any(file.endswith(ext) for ext in valid_extensions)]
+loaded_files = dict.fromkeys(files, None)
 
-    def display_image(self, index):
-        filenames = list(self.loaded_files.keys())
-        if index >= 0 and index < len(filenames):
-            filename = filenames[index]
-            img = self.loaded_files[filename]
-            width = int(img.width() * self.zoom_factor)
-            height = int(img.height() * self.zoom_factor)
-            img = img.resize((width, height))
-            self.image_label.config(image=img)
-            self.image_label.image = img  # Keep a reference to prevent garbage collection
-            metadata = [str(self.get_exif(filename, tag)) for tag in
-                        ["FocalLength", "FNumber", "ExposureTime", "ISOSpeedRatings", "ExposureBiasValue",
-                         "ExposureProgram", "DateTime", "LensModel"]]
-            metadata_labels = ["FocalLength: ", "FNumber: ", "ExposureTime: 1/", "ISO-", "Exposure Bias: ",
-                               "Bracketing: ", "DateTime: ", "Lens: "]
-            metadata_text = "\n".join(label + data for label, data in zip(metadata_labels, metadata))
-            self.file_info_text.set(metadata_text)
-            self.root.update()
-            self.root.title(f"Eu amo o Bernado - {filename} - {index + 1} of {len(self.loaded_files)}")
-        else:
-            messagebox.showinfo("Image Not Found", f"The image '{filename}' was not found in the loaded files.")
 
-    def get_exif(self, filename, tag):
-        img = Image.open(os.path.join(self.source_dir, filename))
-        try:
-            exif_table = {}
-            for k, v in img._getexif().items():
-                exif_tag = ExifTags.TAGS.get(k)
-                exif_table[exif_tag] = v
-            return exif_table.get(tag)
-        except AttributeError:
-            print(f"Error: Failed to extract exif tags from file {filename}")
-        except KeyError:
-            print(f"Info: Key 'Orientation' of {filename} was not found")
+# Extract Exif metadata from an image file
+def exif(filename, gettag):
+    img = Image.open(os.path.join(source_dir, filename))
+    try:
+        exif_table = {}
+        for k, v in img._getexif().items():
+            tag = TAGS.get(k)
+            exif_table[tag] = v
+        return exif_table.get(gettag)
+    except (AttributeError, KeyError):
         return None
 
-    def copy_image(self, event):
-        if self.index_atual >= 0 and self.index_atual < len(self.loaded_files):
-            filename = list(self.loaded_files.keys())[self.index_atual]
-            copia_arquivo(self.source_dir, filename, self.destination_dir)
-            self.next_image(event)
+
+# Load an image and perform necessary adjustments
+def carrega(filename):
+    img = Image.open(os.path.join(source_dir, filename))
+
+    try:
+        Orientation = exif(filename, "Orientation")
+        if Orientation == 8:
+            img = img.rotate(90, expand=True)
+        elif Orientation == 3:
+            img = img.rotate(180, expand=True)
+        elif Orientation == 6:
+            img = img.rotate(270, expand=True)
+    except KeyError:
+        pass
+
+    aspect_ratio = img.width / img.height
+    screen_width = root.winfo_screenwidth()
+    screen_height = root.winfo_screenheight()
+    screen_aspect_ratio = screen_width / screen_height
+
+    if aspect_ratio < screen_aspect_ratio:
+        img = img.resize((int(screen_height * aspect_ratio), screen_height), Image.LANCZOS)
+    else:
+        img = img.resize((screen_width, int(screen_width / aspect_ratio)), Image.LANCZOS)
+
+    return ImageTk.PhotoImage(img)
+
+def get_image_metadata(file):
+    metadata_tags = ["FocalLength",    "FNumber",     "ExposureTime", "ISOSpeedRatings", "ExposureBiasValue", "ExposureProgram", "DateTime", "LensModel"]
+    metadata_labels = ["FocalLength: ", "FNumber: ", "ExposureTime: 1/", "ISO-",         "Exposure Bias: ",   "Bracketing: ", "DateTime: ", "Lens: "]
+    metadata = [str(exif(file, tag)) for tag in metadata_tags]
+    # Fix for ExposureMode == 2 is bracketed
+    metadata[2] = str(1 / float(metadata[2]))
+    formatted_metadata = [label + value if value else "" for label, value in zip(metadata_labels, metadata)]
+    return '\n'.join(formatted_metadata)
+
+
+def mostra_imagem(file):
+    if file in loaded_files:
+        imagem_mostrada['image'] = loaded_files[file]
+        if file_info_visible:
+            file_info_label.place(anchor=tk.NW)
+            metadata = get_image_metadata(file)
+            file_info_text.set(metadata)
         else:
-            messagebox.showinfo("Invalid Image", "No image selected to copy.")
+            file_info_label.place_forget()
+        root.update()
+        root.title("Eu amo o Bernado - " + str(files[index_atual]) + " - " + str(index_atual + 1) + " of " + str(
+            len(files)))
+    else:
+        print("This didn't work. Info for debugging follows:")
+        print(f"index_atual: {index_atual}")
+        print(f"files[index_atual]: {files[index_atual]}")
+        print(f"loaded_files[files[index_atual]] was not found")
+        print("These are the available dictionary keys:", loaded_files.keys())
 
-    def next_image(self, event):
-        self.index_atual = (self.index_atual + 1) % len(self.loaded_files)
-        self.display_image(self.index_atual)
+    if True:
+        try:
+            img = Image.open(os.path.join(source_dir, file))
+        except FileNotFoundError:
+            print(f"Error: {file} not found")
+            return
+        except OSError as e:
+            print(f"Error opening file: {e}")
+            return
+        try:
+            ExposureMode = exif(file, "ExposureMode")
+            if ExposureMode == 0 or ExposureMode == 8:
+                root.configure(background="black")
+            elif ExposureMode == 2:
+                root.configure(background="gray")
+        except KeyError:
+            pass
+    return
 
-    def previous_image(self, event):
-        self.index_atual = (self.index_atual - 1) % len(self.loaded_files)
-        self.display_image(self.index_atual)
+# Event handlers
+def copiar(event):
+    copia_arquivo(source_dir, files[index_atual], destination_dir)
+    proxima(event)
+    if loaded_files.get(files[(index_atual + 1) % len(files)]) is None:
+        loaded_files[files[index_atual + 1]] = carrega(files[index_atual + 1])
 
-    def zoom_image(self, event):
-        if event.delta > 0:
-            self.zoom_factor *= 1.2  # Increase zoom factor by 20%
-        else:
-            self.zoom_factor /= 1.2  # Decrease zoom factor by 20%
-        self.display_image(self.index_atual)
 
-if __name__ == "__main__":
-    root = tk.Tk()
-    image_viewer = ImageViewer(root)
-    image_viewer.setup_gui()
-    image_viewer.run()
+def proxima(event):
+    global index_atual
+    index_atual = (index_atual + 1) % len(files)
+    mostra_imagem(files[index_atual])
+
+
+def anterior(event):
+    global index_atual
+    index_atual = (index_atual - 1) % len(files)
+    mostra_imagem(files[index_atual])
+
+
+def right(event):
+    proxima(event)
+    if loaded_files.get(files[(index_atual + 1) % len(files)]) is None:
+        loaded_files[files[index_atual + 1]] = carrega(files[index_atual + 1])
+
+
+def left(event):
+    anterior(event)
+    if loaded_files.get(files[(index_atual - 1) % len(files)]) is None:
+        loaded_files[files[index_atual - 1]] = carrega(files[index_atual - 1])
+
+
+# Bind keys to event handlers
+root.bind("<Up>", copiar)
+root.bind("<Right>", right)
+root.bind("<Left>", left)
+root.bind("6", right)
+root.bind("4", left)
+root.bind("8", copiar)
+root.bind("i", toggle_file_info)
+
+# Preload the first two images
+try:
+    for file in files[0:2]:
+        loaded_files[file] = carrega(file)
+except FileNotFoundError as e:
+    print(f"Error: {e}")
+except OSError as e:
+    print(f"Error opening file: {e}")
+except Exception as e:
+    print(f"Error: {e}")
+
+# Show the first image
+mostra_imagem(files[0])
+
+root.mainloop()
+print("fim")
