ef2c988e9616ddd61b79fd2033b69fef7560bc02
Eltink <glaucoeltink.97@gmail.com>
2023-10-28 15:26:08 +0200

    Updating to status v3

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-# Tendando compartimentalizar as funcoes
+# Tava zoando com o chat GPT e testando as saidas. Não ta funcional
+
 import tkinter as tk
+import threading
+import time
 from PIL import Image, ImageTk
 import os
-import funcoes #nao existe mais, era um arquivo vazio
 from copiador_de_arquivo import copia_arquivo
-import time
 
 # Inicializando as coisas globais chatas
 root = tk.Tk()  # Create window
@@ -12,71 +13,75 @@ imagem_mostrada = tk.Label(root)
 index_atual = 0  # Qual o indice da imagem que esta sendo mostrada
 
 # Import of configuration parameters
-source_dir = 'C:\\Users\\glauc\\Desktop\\Selecionar\\Cel pai\\JPG'
+source_dir = 'C:\\Users\\glauc\\Desktop\\dest'
 # source_dir = 'C:\\Users\\glauc\\Desktop\\TL raios takaoka\\LR_out'
 # source_dir = 'C:\\Users\\glauc\\Desktop\\Selecionar\\H2\\14421013'
 destination_dir = 'C:\\Users\\glauc\\Desktop\\dest'
+loaded_files = {}  # Dicionario dizendo quais imagens estão guardadas
 
-files = os.listdir(source_dir)  # O nome das imagens
-loaded_files = {}               # Dicionario dizendo quais imagens estão guardadas
 
 def carrega(filename):
-    img = Image.open(os.path.join(source_dir, filename))                    # Arquivo de origem(src+filename)
-    img = img.resize((root.winfo_screenwidth(), root.winfo_screenheight())) # Faz a imagem preencher a tela, nem mais nem menos
-    return ImageTk.PhotoImage(img)                                          # Mostrar a imagem selecionada na tela
-#t = time.time()
-for file in files[0:5]:                 # Carrega so as 2 primeiras imagens, pra ir mais rapido
-    loaded_files[file] = carrega(file)  # O indice do dicio (key) é o nome do arquivo (file) e a "definicao do dicio" é a saída de carrega(file)
-#print(time.time() - t)
-if True: # So that code folding is possible
-    def mostra_imagem(file):
-        if file in loaded_files:
-            imagem_mostrada['image'] = loaded_files[file]  # Mostrar a imagem selecionada na tela
-        else:
-            print("This didnt work. Info for debbug follows:")
-            print(f"index_atual: {index_atual}")
-            print(f"files[index_atual]: {files[index_atual]}")
-            print(f"loaded_files[files[index_atual]] was not found")
-            print("This are the available dictionary keys: ", loaded_files.keys())
-        return
+    img = Image.open(os.path.join(source_dir, filename))  # Arquivo de origem(src+filename)
+    img = img.resize((root.winfo_screenwidth(), root.winfo_screenheight()))
+    loaded_files[filename] = ImageTk.PhotoImage(img)  # Mostrar a imagem selecionada na tela
 
-    def copiar(event):
-        copia_arquivo(source_dir, files[index_atual], destination_dir)
-        proxima(event)
-        return
 
-    def proxima(event):
-        global index_atual
+def carrega_threaded(filename):
+    thread = threading.Thread(target=carrega, args=(filename,))
+    thread.start()
 
-        # Incrementa o index e mostra a proxima
-        mostra_imagem(files[index_atual])
 
-        # Se tiver perto da margem, carrega mais e deleta antigas
-        if files[index_atual] not in loaded_files:
-            for file in files[index_atual:(index_atual + 5) % len(files)]:
-                loaded_files[file] = carrega(file)
+# Função para mostrar
+def mostra_imagem():
+    while files[index_atual] not in loaded_files:
+        time.sleep(0.1)
+    imagem_mostrada['image'] = loaded_files[files[index_atual]]  # Mostrar a imagem selecionada na tela
+    return
 
-        index_atual = index_atual + 1
-        index_atual = index_atual % len(files)  # Depois da ultima, volta pro começo
-        return
 
-    def anterior(event):
-        global index_atual
-        index_atual = index_atual - 1
-        index_atual = index_atual % len(files)  # Depois da ultima, volta pro começo
-        mostra_imagem(files[index_atual])
-        return
+def copiar(event):
+    copia_arquivo(source_dir, files[index_atual], destination_dir)
+    proxima(event)
+    return
+
+
+def proxima(event):
+    global index_atual
+
+    # Se tiver perto da margem, carrega mais e deleta antigas
+    if files[index_atual] not in loaded_files:
+        for file in files[index_atual:(index_atual+5) % len(files)]:
+            carrega_threaded(file)
+
+    # Incrementa o index e mostra a anterior
+    index_atual = index_atual + 1
+    index_atual = index_atual % len(files)  # Depois da ultima, volta pro começo
+    mostra_imagem()
+    return
+
+
+def anterior(event):
+    global index_atual
+    index_atual = index_atual - 1
+    index_atual = index_atual % len(files)  # Depois da ultima, volta pro começo
+    mostra_imagem()
+    return
+
 
 # Setting up overall environment
 root.title("Eu amo o Bernado")
 root.state('zoomed')
 
-mostra_imagem(files[0])  # Inicializando a janela, com a primeira imagem
+files = os.listdir(source_dir)  # O nome das imagens
+
+for file in files:
+    carrega_threaded(file)
+
+mostra_imagem()  # Inicializando a janela, com a primeira imagem
 imagem_mostrada.pack()
 
-root.bind("1",          copiar)
-root.bind("<Right>",    proxima)
-root.bind("<Left>",     anterior)
+root.bind("1", copiar)
+root.bind("<Right>", proxima)
+root.bind("<Left>", anterior)
 
-root.mainloop() # Fica escutando se algum evento aconteceu
-# Qualquer coisa depois disso não vai rodar nunca
+root.mainloop()
