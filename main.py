345362cb536e37f092f10aeac3cb21b9d1dc3c8d
Eltink <glaucoeltink.97@gmail.com>
2023-10-28 15:44:11 +0200

    Updating to status v11

--- main.py
+++ main.py
@@ -7,13 +7,18 @@ import os
 import rawpy # Necessary to read .arw files
 import imageio # Necessary to read .arw files
 import threading
+import subprocess
+
+# trying to implement to open subfolders
 
 import brackets_sorter
 from copiador_de_arquivo import copia_arquivo
 from tools import *
 
-debugging = 0
+debugging = 1
 timing = 0
+show_all = 0
+include_sub_dirs = 1
 
 # Initialize the GUI
 root = tk.Tk()
@@ -37,7 +42,7 @@ file_info_label = tk.Label(root, textvariable=file_info_text, font=('Arial', 12)
 feedback_label_text = tk.StringVar()
 feedback_label_text.set("init")
 feedback_label = tk.Label(root, textvariable=feedback_label_text, font=('Arial', 12))
-feedback_label.place(anchor=tk.SW)
+feedback_label.place(y=200, anchor=tk.NW)
 
 # Toggle file information display
 def toggle_file_info(event):
@@ -49,26 +54,44 @@ def toggle_file_info(event):
         file_info_label.place_forget()
 
 # Get the source directory from user input
-if debugging: source_dir = r"C:\Users\glauc\Desktop\2023_06_27_Israel_jpgs\Ibex"
-#if debugging: source_dir = filedialog.askdirectory()
+if debugging: source_dir = r"C:\Users\glauc\Desktop\kjaf"
 else: source_dir = filedialog.askdirectory()
 destination_dir = get_destination_dir(source_dir)
 
 # Define valid image file extensions
 valid_extensions = [".JPG", ".jpg", ".jpeg", ".png", ".gif", ".ARW"]
 t0 = time.time()
+
 # Original list of files with valid extensions
-files = [file for file in os.listdir(source_dir) if any(file.endswith(ext) for ext in valid_extensions)]
+# if not include_sub_dirs: files = [file for file in os.listdir(source_dir) if any(file.endswith(ext) for ext in valid_extensions)]
+# else: files = [os.path.join(root, filename) for root, _, filenames in os.walk(source_dir) for filename in filenames if any(filename.endswith(ext) for ext in valid_extensions]
+
+#filepaths = ([os.path.join(root, filename) for root, _, filenames in os.walk(source_dir) for filename in filenames if any(filename.endswith(ext) for ext in valid_extensions])
+
+#files = [os.path.join(root, filename) for  filenames in os.walk(source_dir) for filename in filenames if any(filename.endswith(ext) for ext in valid_extensions]
+filepaths = []
+print (source_dir)
+for path, subdir, files in os.walk(source_dir):
+   for name in files:
+       print(name)
+       if any(name.endswith(ext) for ext in valid_extensions):
+        filepaths.append(os.path.join(path,name))
+
+print("filepaths", filepaths)
+
 
 # Refining files
 filepaths = [os.path.join(source_dir, file) for file in files]
 t1 = time.time()
-showable = is_it_showable(filepaths)  # Example: [1, 1, 0, 1]
+
+if show_all: showable = [1]*len(filepaths)
+else:        showable = is_it_showable(filepaths)  # Example: [1, 1, 0, 1]
+
 if timing: print("Time to calculate showable: ", time.time()-t1)
 files = [file for file, is_showable in zip(files, showable) if is_showable]
 if timing: print("Time to calculate files: ", time.time()-t0)
 
-loaded_files = dict.fromkeys(files, None)
+loaded_files = dict.fromkeys(filepaths, None)
 copied_files = {}
 
 # Load an image and perform necessary adjustments
@@ -119,7 +142,7 @@ def get_image_metadata(file):
             metadata_labels[2] += " 1/"
         else: metadata[2] = str(round(float(metadata[2]))) + "s"
 
-        if metadata[5] == "0": metadata[5] = "Auto exposure"
+        if metadata[5] == "0": metadata[5] = "Single"
         elif metadata[5] == "2": metadata[5] = "Bracketing"
         else: metadata[5] = str(metadata[5]) + " (unknown case, please investigate)"
         formatted_metadata = [label + value if value else "" for label, value in zip(metadata_labels, metadata)]
@@ -136,7 +159,10 @@ def mostra_imagem(file):
             metadata = get_image_metadata(file)
             file_info_text.set(metadata)
         else: file_info_label.place_forget()
-        feedback_label.pack()
+        # feedback_label.place_forget()
+        # todo How do i remove the feedback label when not needed?
+        # feedback_label.pack()
+        # No idea why i put this pack here, but without it its working
 
         root.update()
         root.title("Eu amo o Bernado - " + str(files[index_atual]) + " - " + str(index_atual + 1) + " of " + str(len(files)))
@@ -167,18 +193,25 @@ def copiar(event):
         copia_arquivo(source_dir, filename, destination_dir)
         copied_files[filename] = True
         if get_exif(filepath, "ExposureMode") != 2: # Isnt bracketed
-            feedback_label_text.set("Copied 1 unbracketed file")
+            feedback_label_text.set(f"Copied {filename}")
+
         elif get_exif(filepath, "ExposureMode") == 2: # Is bracketed
-            feedback_label_text.set("Copied median")
+            feedback_label_text.set(f"Copied median {filename}")
             EV = get_exif(filepath, "ExposureBiasValue")
+
             darker = file_navigator(filepath, -1)
-            if get_exif(darker, "ExposureBiasValue") in [EV - 3, EV - 2, EV - 1]:
+            if round(get_exif(darker, "ExposureBiasValue"), 1) in [EV - 3, EV - 2, EV - 1]:
                 copia_arquivo(source_dir, darker, destination_dir) # How is this working, if source and darker are paths?
                 feedback_label_text.set(feedback_label_text.get()+" and darker")
+            else: feedback_label_text.set(feedback_label_text.get()+" but no darker")
+
             lighter = file_navigator(filepath, +1)
-            if get_exif(lighter, "ExposureBiasValue") in [EV + 3, EV + 2, EV + 1]:
+            if round(get_exif(lighter, "ExposureBiasValue"), 1) in [EV + 3, EV + 2, EV + 1]:
                 copia_arquivo(source_dir, lighter, destination_dir)
                 feedback_label_text.set(feedback_label_text.get() + " and lighter")
+            else: feedback_label_text.set(feedback_label_text.get() + " but no lighter")
+
+            if feedback_label_text.get().endswith("and darker and lighter"): feedback_label_text.set(f"Copied 3x {filename}")
 
         right(event)
 
@@ -203,7 +236,8 @@ def left(event):
         carrega(files[index_atual - 1])
 
 def exit_feedback(event):
-    tk.messagebox.showinfo("Copying Complete", "Image copying process completed.")
+    tk.messagebox.showinfo("Copying Complete", "Image copying process completed.\nPress Enter to close")
+    root.destroy()
 
 # Bind keys to event handlers
 root.bind("<Up>", copiar)
@@ -227,9 +261,29 @@ if timing: print("Time to load second image: ", time.time()-t2)
 if timing: print("Time to boot program: ", time.time()-t0)
 
 
+def open_in_explorer(event):
+    subprocess.Popen(['explorer', source_dir])
+root.bind("<Control-e>", open_in_explorer)  # Binds Control+e to open in Explorer
+open_in_explorer_button = tk.Button(root, text="Open in Explorer", command=open_in_explorer)
+open_in_explorer_button.pack()
+
+
+def open_with_photos(_=None): # I dont understand whats the difference between this and event as an argument
+    if index_atual >= 0 and index_atual < len(files):
+        file_path = os.path.join(source_dir, files[index_atual])
+        os.system(f'start "" "{file_path}"')  # Opens the file with the default associated program
+    else:
+        # Handle the case where index_atual is out of bounds
+        pass  # You can display an error message or take other appropriate action here
+
+
+root.bind("<Control-r>", open_with_photos)  # Binds Control+r to open with Windows Photos
+
 slideshow_event = threading.Event()
 slideshow_thread = None
 slideshow_running = False
+
+
 def start_slideshow(event=None):  # Add the event parameter with a default value of None
     global index_atual, slideshow_running, slideshow_thread
 
@@ -260,15 +314,17 @@ root.bind("s", start_slideshow)
 
 # WIP not working yet
 # Define a global flag and thread to track and interrupt the loading process
-#loading_thread = None
-#loading_in_progress = False
-#terminate_loading = False  # Flag to signal thread should be terminated
+# loading_thread = None
+# loading_in_progress = False
+# terminate_loading = False  # Flag to signal thread should be terminated
 #
 # Define your global variables here
 loading_in_progress = False
 loading_thread = None
 loaded_count = 0
 stop_loading_event = threading.Event()  # Event to signal the loading thread to stop
+
+
 def load_all_images(event=None):
     global index_atual, loading_in_progress, loading_thread, loaded_count
 
@@ -312,21 +368,20 @@ def load_all_images(event=None):
                 elapsed_time = current_time - start_time
 
                 if loaded_count > 0:
-                    avg_time_per_image = elapsed_time / loaded_count
+                    avg_time_per_image = round(elapsed_time / loaded_count, 2)
                     estimated_remaining_time = avg_time_per_image * (total_images - loaded_count)
                 else:
                     avg_time_per_image = 0
                     estimated_remaining_time = 0
 
-                if estimated_remaining_time > 60:
-                    estimated_remaining_time_text = f"Estimated Remaining Time: {estimated_remaining_time:.2f} seconds"
+                if estimated_remaining_time < 60:
+                    estimated_remaining_time_text = f"Estimated Remaining Time: {estimated_remaining_time:.0f} seconds"
                 else:
                     estimated_remaining_time_text = f"Estimated Remaining Time: {estimated_remaining_time/60:.1f} minutes"
 
                 progress_label.config(text=f"Progress: {progress:.2f}%\n"
-                                           f"Elapsed Time: {elapsed_time:.2f} seconds\n"
                                            f"Average Time per Image: {avg_time_per_image:.3f} seconds\n"
-                                           f"{estimated_remaining_time_text}")
+                                           f"{estimated_remaining_time_text}") #f"Elapsed Time: {elapsed_time:.2f} seconds\n"
                 progress_window.update()
 
             loading_in_progress = False  # Reset the flag
@@ -335,11 +390,14 @@ def load_all_images(event=None):
         loading_thread = threading.Thread(target=loading_thread_func)
         loading_thread.start()
 
+
 # Bind the "l" key to the load_all_images function
 root.bind("l", load_all_images)
+
+
 def load_some_images(event=None):
     global index_atual, loaded_files
-    #num_images = 20   # Number of images to be loaded
+    # num_images = 20   # Number of images to be loaded
     allowed_time = 5  # Seconds to run the function for
     progress_window = tk.Toplevel(root)
     progress_window.title("Loading Images Progress")
@@ -372,4 +430,5 @@ root.bind("L", load_some_images)
 root.mainloop()
 print("fim")
 
+# TODO check copiar, its not working with brackets
 # todo if sel is empty delete
