a346b671cec5bd43e888f768dca05bf983c1484d
Eltink <glaucoeltink.97@gmail.com>
2023-11-13 21:37:47 +0100

    Bug fixes

--- anubis.py
+++ anubis.py
@@ -3,10 +3,10 @@ import copiador_de_arquivo
 
 # Programm to sort between good and bar HDRs, previously sorted
 
-bad_dir = r"C:\Users\glauc\Desktop\bom ou bad\Bad\copiadas"
+bad_dir = r"C:\Users\glauc\Desktop\bom ou bad\bad"
 bad = os.listdir(bad_dir)
 
-all_dir = r"C:\Users\glauc\Desktop\bom ou bad\All HDRs"
+all_dir = r"C:\Users\glauc\Desktop\bom ou bad\all"
 all = os.listdir(all_dir)
 
 i = 0
@@ -15,13 +15,18 @@ bons, bads = 0, 0
 dest_dir = r"C:\Users\glauc\Desktop\bom ou bad\out"
 
 # This assumes that all bad brackets are complete!
-for i in range(len(all)):
-    if all[i] in bad:
-        copiador_de_arquivo.copia_arquivo(all_dir, all[i+1], dest_dir)
+while i < len(all):
+    if i == len(all)-1:
+        if all[i-1] not in bad:  # This assumes that the last good is complete
+            copiador_de_arquivo.copia_arquivo(all_dir, all[i], r"C:\Users\glauc\Desktop\bom ou bad\out\bom")
+            i += 1
+            bons += 1
+    elif all[i+1] in bad:
+        copiador_de_arquivo.copia_arquivo(all_dir, all[i+1], r"C:\Users\glauc\Desktop\bom ou bad\out\bad")
         i += 3
         bads += 1
     else:
-        copiador_de_arquivo.copia_arquivo(all_dir, all[i], dest_dir)
+        copiador_de_arquivo.copia_arquivo(all_dir, all[i], r"C:\Users\glauc\Desktop\bom ou bad\out\bom")
         i += 1
         bons += 1
 
