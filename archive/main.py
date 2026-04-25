2540cb61e0b8b0b821b93487db7454ab9f70306b
Eltink <64210683+Eltink@users.noreply.github.com>
2026-04-25 16:48:31 +0200

    Archive: Rewrite main script as a Tkinter GUI application (main v3.0.0.py)

--- archive/main.py
+++ archive/main.py
@@ -1,26 +1,87 @@
-from threading import Thread
+# Tava zoando com o chat GPT e testando as saidas. Não ta funcional
 
-class CARRO():
-    def __init__(self,ano,name):
-        self.ano = ano
-        self.name = name
+import tkinter as tk
+import threading
+import time
+from PIL import Image, ImageTk
+import os
+from copiador_de_arquivo import copia_arquivo
+
+# Inicializando as coisas globais chatas
+root = tk.Tk()  # Create window
+imagem_mostrada = tk.Label(root)
+index_atual = 0  # Qual o indice da imagem que esta sendo mostrada
+
+# Import of configuration parameters
+source_dir = 'C:\\Users\\glauc\\Desktop\\dest'
+# source_dir = 'C:\\Users\\glauc\\Desktop\\TL raios takaoka\\LR_out'
+# source_dir = 'C:\\Users\\glauc\\Desktop\\Selecionar\\H2\\14421013'
+destination_dir = 'C:\\Users\\glauc\\Desktop\\dest'
+loaded_files = {}  # Dicionario dizendo quais imagens estão guardadas
 
 
-    def set_name(self,name):
-        self.name = name
-        print(self.name)
-
-    def get_name(self):
-        return(self.name)
+def carrega(filename):
+    img = Image.open(os.path.join(source_dir, filename))  # Arquivo de origem(src+filename)
+    img = img.resize((root.winfo_screenwidth(), root.winfo_screenheight()))
+    loaded_files[filename] = ImageTk.PhotoImage(img)  # Mostrar a imagem selecionada na tela
 
 
+def carrega_threaded(filename):
+    thread = threading.Thread(target=carrega, args=(filename,))
+    thread.start()
 
-def main():
-    carro = CARRO(1997,"Onix")
-    print(carro.get_name())
-    carro.set_name("Corvette")
-    print(carro.get_name())
-    t = Thread (target=carro.set_name,args=["Novo"])
-    t.run()
 
-main()
+# Função para mostrar
+def mostra_imagem():
+    while files[index_atual] not in loaded_files:
+        time.sleep(0.1)
+    imagem_mostrada['image'] = loaded_files[files[index_atual]]  # Mostrar a imagem selecionada na tela
+    return
+
+
+def copiar(event):
+    copia_arquivo(source_dir, files[index_atual], destination_dir)
+    proxima(event)
+    return
+
+
+def proxima(event):
+    global index_atual
+
+    # Se tiver perto da margem, carrega mais e deleta antigas
+    if files[index_atual] not in loaded_files:
+        for file in files[index_atual:(index_atual+5) % len(files)]:
+            carrega_threaded(file)
+
+    # Incrementa o index e mostra a anterior
+    index_atual = index_atual + 1
+    index_atual = index_atual % len(files)  # Depois da ultima, volta pro começo
+    mostra_imagem()
+    return
+
+
+def anterior(event):
+    global index_atual
+    index_atual = index_atual - 1
+    index_atual = index_atual % len(files)  # Depois da ultima, volta pro começo
+    mostra_imagem()
+    return
+
+
+# Setting up overall environment
+root.title("Eu amo o Bernado")
+root.state('zoomed')
+
+files = os.listdir(source_dir)  # O nome das imagens
+
+for file in files:
+    carrega_threaded(file)
+
+mostra_imagem()  # Inicializando a janela, com a primeira imagem
+imagem_mostrada.pack()
+
+root.bind("1", copiar)
+root.bind("<Right>", proxima)
+root.bind("<Left>", anterior)
+
+root.mainloop()
