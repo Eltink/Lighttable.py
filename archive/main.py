3e25a58713d9eac827974fdd799ab2a666be5e1c
Eltink <64210683+Eltink@users.noreply.github.com>
2026-04-25 16:48:31 +0200

    Archive: Experimenting with Object Oriented Programming and threading (OOP.py)

--- archive/main.py
+++ archive/main.py
@@ -1,31 +1,26 @@
-import os
-import shutil
+from threading import Thread
 
-# Path to the directory containing the *.arw files
-arw_dir = "path/to/arw/files"
+class CARRO():
+    def __init__(self,ano,name):
+        self.ano = ano
+        self.name = name
 
-# Path to the directory containing the wanted *.jpg files
-jpg_dir = "path/to/jpg/files"
 
-# Create a new directory called "copied"
-copied_dir = "copied"
-os.makedirs(copied_dir, exist_ok=True)
+    def set_name(self,name):
+        self.name = name
+        print(self.name)
 
-# List to store the names of the *.jpg files that do not have an associated *.arw file
-not_found = []
+    def get_name(self):
+        return(self.name)
 
-# Iterate over the files in the *.arw directory
-for arw_file in os.listdir(arw_dir):
-    # Check if the file is an *.arw file
-    if arw_file.endswith(".arw"):
-        # Get the corresponding *.jpg file by replacing the file extension
-        jpg_file = arw_file.replace(".arw", ".jpg")
 
-        # Check if the corresponding *.jpg file exists in the jpg_dir directory
-        if os.path.exists(os.path.join(jpg_dir, jpg_file)):
-            # Copy the *.arw file to the "copied" directory
-            shutil.copy(os.path.join(arw_dir, arw_file), copied_dir)
-            # Copy the corresponding *.jpg file to the "copied" directory
-            shutil.copy(os.path.join(jpg_dir, jpg_file), copied_dir)
-        else:
-    # If the corresponding *.jpg file does not exist, add its name to the
+
+def main():
+    carro = CARRO(1997,"Onix")
+    print(carro.get_name())
+    carro.set_name("Corvette")
+    print(carro.get_name())
+    t = Thread (target=carro.set_name,args=["Novo"])
+    t.run()
+
+main()
