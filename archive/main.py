935f0e091c6323fa8a5e77abc6f2d1879ac06f50
Eltink <64210683+Eltink@users.noreply.github.com>
2026-04-25 16:48:32 +0200

    Archive: Import legacy v2 version for reference (main_v2.py)

--- archive/main.py
+++ archive/main.py
@@ -1,8 +1,8 @@
 # Tendando compartimentalizar as funcoes
 import tkinter as tk
 from PIL import Image, ImageTk
-from PIL.ExifTags import TAGS
 import os
+import funcoes
 from copiador_de_arquivo import copia_arquivo
 import time
 
@@ -12,64 +12,26 @@ imagem_mostrada = tk.Label(root)
 index_atual = 0  # Qual o indice da imagem que esta sendo mostrada
 
 # Import of configuration parameters
-source_dir = 'C:\\Users\\glauc\\Desktop\\kjaf'
+source_dir = 'C:\\Users\\glauc\\Desktop\\Selecionar\\Cel pai\\JPG'
 # source_dir = 'C:\\Users\\glauc\\Desktop\\TL raios takaoka\\LR_out'
 # source_dir = 'C:\\Users\\glauc\\Desktop\\Selecionar\\H2\\14421013'
-destination_dir = 'C:\\Users\\glauc\\Desktop\\insta'
+destination_dir = 'C:\\Users\\glauc\\Desktop\\dest'
 
 files = os.listdir(source_dir)  # O nome das imagens
-#loaded_files = {}               # Dicionario dizendo quais imagens estão guardadas
-loaded_files = dict.fromkeys(files, None)
-
-# def carrega(filename):
-#     img = Image.open(os.path.join(source_dir, filename))                    # Arquivo de origem(src+filename)
-#     img = img.resize((root.winfo_screenwidth(), root.winfo_screenheight())) # Faz a imagem preencher a tela, nem mais nem menos
-#     return ImageTk.PhotoImage(img)                                          # Mostrar a imagem selecionada na tela
+loaded_files = {}               # Dicionario dizendo quais imagens estão guardadas
 
 def carrega(filename):
-    img = Image.open(os.path.join(source_dir, filename))
-    exif_table = {}
-    for k, v in img._getexif().items():
-        tag = TAGS.get(k)
-        exif_table[tag] = v
-    print(exif_table)
-    if exif_table["Orientation"] == 8:
-        img = img.rotate(90, expand=True)
-        print("virou")
-        # Rotate the image if it is in portrait orientation
-
-    # Get aspect ratio of the image
-    aspect_ratio = img.width / img.height
-
-    # Get the screen height and width
-    screen_width = root.winfo_screenwidth()
-    screen_height = root.winfo_screenheight()
-
-    # Get the screen aspect ratio
-    screen_aspect_ratio = screen_width / screen_height
-
-    # Determine the new size for the image
-    if aspect_ratio < screen_aspect_ratio:
-        new_width = int(screen_height * aspect_ratio)
-        new_height = screen_height
-    else:
-        new_width = screen_width
-        new_height = int(screen_width / aspect_ratio)
-
-    # Resize the image to the new size
-    img = img.resize((new_width, new_height))
-
-    return ImageTk.PhotoImage(img)
-
-
-for file in files[0:2]:                 # Carrega so as 2 primeiras imagens, pra ir mais rapido
+    img = Image.open(os.path.join(source_dir, filename))                    # Arquivo de origem(src+filename)
+    img = img.resize((root.winfo_screenwidth(), root.winfo_screenheight())) # Faz a imagem preencher a tela, nem mais nem menos
+    return ImageTk.PhotoImage(img)                                          # Mostrar a imagem selecionada na tela
+#t = time.time()
+for file in files[0:5]:                 # Carrega so as 2 primeiras imagens, pra ir mais rapido
     loaded_files[file] = carrega(file)  # O indice do dicio (key) é o nome do arquivo (file) e a "definicao do dicio" é a saída de carrega(file)
-
+#print(time.time() - t)
 if True: # So that code folding is possible
     def mostra_imagem(file):
         if file in loaded_files:
             imagem_mostrada['image'] = loaded_files[file]  # Mostrar a imagem selecionada na tela
-            root.update()                                   #Aparently this is not ideal performance, but is already an improvement
         else:
             print("This didnt work. Info for debbug follows:")
             print(f"index_atual: {index_atual}")
@@ -85,60 +47,36 @@ if True: # So that code folding is possible
 
     def proxima(event):
         global index_atual
+
         # Incrementa o index e mostra a proxima
-        index_atual = index_atual + 1
         mostra_imagem(files[index_atual])
+
+        # Se tiver perto da margem, carrega mais e deleta antigas
+        if files[index_atual] not in loaded_files:
+            for file in files[index_atual:(index_atual + 5) % len(files)]:
+                loaded_files[file] = carrega(file)
+
+        index_atual = index_atual + 1
         index_atual = index_atual % len(files)  # Depois da ultima, volta pro começo
         return
 
     def anterior(event):
         global index_atual
         index_atual = index_atual - 1
-        mostra_imagem(files[index_atual])
         index_atual = index_atual % len(files)  # Depois da ultima, volta pro começo
-        return
-
-    def right(event):
-        t = time.time()
-        proxima(event)
-        print(time.time()-t, "s para proxima")
-        t = time.time()
-        if loaded_files.get(files[index_atual + 1] ) is None: #% len(files)
-            loaded_files[files[index_atual+1]] = carrega(files[index_atual+1])
-        print(time.time() - t, "s para load index=",index_atual)
-        return
-
-    def left(event):
-        anterior(event)
-        if loaded_files.get(files[index_atual - 1] ) is None: #% len(files)
-            loaded_files[files[index_atual-1]] = carrega(files[index_atual-1])
+        mostra_imagem(files[index_atual])
         return
 
 # Setting up overall environment
 root.title("Eu amo o Bernado")
 root.state('zoomed')
-root.configure(background="black")
 
 mostra_imagem(files[0])  # Inicializando a janela, com a primeira imagem
 imagem_mostrada.pack()
 
-#Old
 root.bind("1",          copiar)
-root.bind("<Right>",    right)
-root.bind("<Left>",     left)
-
-#New
-root.bind("6",          right)
-root.bind("4",          left)
-root.bind("8",          copiar)
-
-#location = os.path.join(source_dir, files[0])
-#from exif import Image
-#with open(location, 'rb') as image_file:
-#    my_image = Image(image_file)
-#my_image.list_all()
-#print(my_image)
+root.bind("<Right>",    proxima)
+root.bind("<Left>",     anterior)
 
 root.mainloop() # Fica escutando se algum evento aconteceu
-# Qualquer coisa depois disso só roda dps q fechar a janela
-print("fim")
+# Qualquer coisa depois disso não vai rodar nunca
